f8667528b42141ec2e19d8d7fcd8178e54e020ac
sarahnash <sarahnash1112@gmail.com>
2018-12-13 19:10:08 -0600

    routes with complete functions

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(express.static(__dirname + '/public'))
+
 var todoList = [
   {
     id: 1,
     todo: 'Implement a REST API'
+  },
+  {
+    id: 2,
+    todo: 'play play play'
   }
 ]
 
@@ -19,27 +25,56 @@ app.get('/api/todos', function (req, res, nextFn) {
 })
 
 // GET /api/todos/:id
-app.get('/api/todos/:id', function (req, res, nextFn) {
-  console.log(req.params.id)
-  res.send(todoList[req.params.id - 1])
+app.get('/api/todos/:id', function (req, res, next) {
+  var x = req.params.id
+  var output = null
+  todoList.map(function (element) {
+    if (parseInt(element.id) === x) {
+      output = element
+    }
+  })
+
+  res.json(output)
+  // res.send(JSON.stringify()) is the same as res.json()
 })
 
 // POST /api/todos
-app.post('/api/todos', function (req, res, nextFn) {
-  console.log(req.body)
-  res.send('req.body is an empty object, how do I put something in it, with a nextFN?')
+app.post('/api/todos', function (req, res, next) {
+  // console.log(req.body);
+  var newID = todoList.length + 1
+  var newTodo = req.body
+  newTodo['id'] = newID
+  // console.log(newTodo);
+  todoList.push(newTodo)
+  // console.log(todoList);
+  res.json(todoList)
 })
 
 // PUT /api/todos/:id
-app.put('/api/todos/:id', function (req, res, nextFn) {
+app.put('/api/todos/:id', function (req, res, next) {
   console.log(req.params.id)
-  res.send('putting something in this req.params.id: ' + req.params.id + ' But what?')
+  var ID = req.params.id
+  console.log(ID)
+  todoList.map(function (element) {
+    if (parseInt(element.id) === ID) {
+      element['isComplete'] = true
+    }
+    console.log(element)
+  })
+  console.log(todoList)
+  res.json(todoList)
 })
 
 // DELETE /api/todos/:id
-app.delete('/api/todos/:id', function (req, res, nextFn) {
-  console.log('you deleted req.params.id: ' + req.params.id)
-  res.send(todoList)
+app.delete('/api/todos/:id', function (req, res, next) {
+  console.log(req.params.id)
+  var ID = req.params.id
+  todoList.map(function (element) {
+    var idx = ID - 1
+    todoList.splice(idx, 1, null)
+  })
+  console.log(todoList)
+  res.json(todoList)
 })
 
 app.listen(3000, function () {
